a6cfd3d7a0366e49406919953cb469a5fb4d4d50
cepulit1 <cepulit1@cs.manchester.ac.uk>
2013-03-09 18:11:06 +0000

    first damn commit

---
+++ Bus.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import static java.util.Calendar.*;
+
+pubic class Bus
+{
+
+  int driverID;
+  String name;
+  //int number;
+  int holidays_taken;
+  int hours_this_year; // ?
+  int  hours_this_week
+
+  public Driver(int requiredID)
+  {
+    driverID = requiredID;
+    name = DriverInfo.getName(requiredID);
+    holidays_taken = DriverInfo.getHolidaysTaken(requiredID);
+    hours_this_year = DriverInfo.getHoursThisYear(requiredID);
+    hours_this_week = DriverInfo.getHoursThisWeek(requiredID);
+
+
+  }
+
+}
