a0d33e9df43455a7a4ef54fb7471f40e503b5e67
cepulit1 <cepulit1@cs.manchester.ac.uk>
2013-03-12 16:08:00 +0000

    Bus.java should be used to set availability of buses for the day. It also provides method for retrieving individual timetable for the bus. The only stats available is the number of days the bus was used that week. BusTest.java is a simple testing example

--- Bus.java
+++ Bus.java
@@ -1,11 +1,5 @@
-/*Issues:
--- java.util.Date or ints for each day?
--- drivers change buses?  
--- what is the level of precision for bus availability? Day, hour, minute?
-
-*/
-import java.util.*;
-import static java.util.Date.*;
+import java.util.Date;
+import java.util.Calendar;
 
 /**
   *
@@ -16,71 +10,83 @@ public class Bus
 {
 
   static int busID;
-   String busNumber; // fleet number
-
-  byte[][] availability; // availability for each day at every minute
-
-  int[] dailyHours;  // hours used on every day
-  int weeklyHours; // hours used on this
+  static String busNumber; // fleet number
+  static int daysUsed; // total number of days the bus was in use.
  
   public Bus(int requiredID)
   {
     busID = requiredID;
     busNumber = BusInfo.busNumber(requiredID);
+    daysUsed = 0;
 
-    availability = new byte [7][1440]; // seven days, each has 1440 minutes
-    dailyHours = new int [7];
   } // Bus constructor
 
+  // availability for today
+  public static boolean isAvailable()
+  {
+    return BusInfo.isAvailable(busID);
+  } // isAvailable
+
+  // availability for some day 
   public static boolean isAvailable(Date date)
   {
     return BusInfo.isAvailable(busID, date);
   } // isAvailable
 
-  public static String getUsageDay(int day)
-  {
-    
-    String usage = "Bus " + busID + " usage for the day " + day + " of the week\n"; 
-   
-/*
-    int lastBusy = 0;
-    for (int i =0; i < 1440; i++)
-    {
-      if
-      boolean currentlyBusy = availability[day][0];
-
-      while(currentlyBusy)
-      {
-        lastBusy = i;
-      } // while
-
-    } // for*/
-    return usage;
-  } // getUsageDay
-
-  public static String getUsageWeek()
-  {
-
-        String usage = "Bus " + busID + " usage for the week:";
-
-    return usage;
-  } // getUsageWeek 
-
+  // sets availability for some day
   public static void setAvailability(Date date, boolean available)
   {
       BusInfo.setAvailable(busID, date, available);
   } // setAvailability
 
+  public static String getDailyUse(Date day)
+  {
+    
+    if (isAvailable(day))
+      return day + " Not in use\n";
+    else
+      return day + " In use\n";
+  } // getDailyUse
 
+  /**
+  *
+  *@param date of the first day of the week
+  *
+  */
+  public static String getWeeklyUse(Date day)
+  {
 
+    String usage = "Bus " + busID + " is used on these days this week: \n"; 
+    Date newDay = day; 
+    Calendar cal = Calendar.getInstance();
+    
+    for (int i = 0; i < 7; i++)
+    {
+      usage += getDailyUse(newDay);
+      cal.setTime(newDay);
+      cal.add(Calendar.DATE, 1);
+      newDay = cal.getTime();
+    } // for
+ 
+    return usage;
+  } // getWeeklyUse 
+
+  public static int getWorkload(Date day)
+  {
+    Date newDay = day; 
+    Calendar cal = Calendar.getInstance();
+    for (int i = 0; i < 7; i++)
+    {
+      if (!isAvailable(newDay))
+        daysUsed++;
+      cal.setTime(newDay);
+      cal.add(Calendar.DATE, 1);
+      newDay = cal.getTime();
+    } // for
+    
+    return daysUsed;
+  } // getWorkload
 } // class Bus
-
-
-
-
-
-
-
 
 
 
