ae7c55f65e05cdcfee95ff12b55a65e4c4a357a1
cepulit1 <cepulit1@cs.manchester.ac.uk>
2013-03-15 11:47:15 +0000

    test13

--- Bus.java
+++ Bus.java
@@ -1,60 +1,26 @@
-<<<<<<< HEAD
 import java.util.*;
 import static java.util.Calendar.*;
 
 /**
   *
-  *@author F16
-=======
-import java.util.Date;
-import java.util.Calendar;
-
-/**
-  *
   *@author Tomas Cepulis, F16
->>>>>>> a0d33e9df43455a7a4ef54fb7471f40e503b5e67
   *
   */
 public class Bus
 {
-<<<<<<< HEAD
-  int busID;
-  int busNumber; // fleet number
-  int[] dailyHours;  // hours used on every day
-  int weeklyHours; // hours used on this
-  TimeTable timetable;
-=======
 
   static int busID;
   static String busNumber; // fleet number
   static int daysUsed; // total number of days the bus was in use.
  
->>>>>>> a0d33e9df43455a7a4ef54fb7471f40e503b5e67
   public Bus(int requiredID)
   {
     busID = requiredID;
     busNumber = BusInfo.busNumber(requiredID);
-<<<<<<< HEAD
-  } // Bus constructor
-  public setTimeTable(BusTimeTable table)
-  {
-   timetable = table;
-  }
-  public boolean checkDailyAvailability(Date date)
-  {
-   return BusInfo.isAvailable(busID, date);
-  }
-  public boolean checkHourlyAvailability(Date date, int startTime, int endTime)
-  {
-    return timetable.checkHourlyAvailability(date.getWeek(), startTime, endTime);
-  }
-
-} // class Bus
-=======
     daysUsed = 0;
-
   } // Bus constructor
 
+ 
   // availability for today
   public static boolean isAvailable()
   {
@@ -122,29 +88,4 @@ public class Bus
   } // getWorkload
 } // class Bus
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> a0d33e9df43455a7a4ef54fb7471f40e503b5e67
+trololo
